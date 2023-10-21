5271620224a1109bdc6acf8769120a1d68a98d12
MeshachArinze <meshacharinze@gmail.com>
2023-10-21 20:13:00 +0100

    commit to main

--- noteapp/src/components/note_list.jsx
+++ noteapp/src/components/note_list.jsx
@@ -1,30 +1,38 @@
-import NoteCard from "./note_card";
+import NoteCard from "./NoteCard.js";
+import notes from "../data/notes.json";
+import { useEffect, useState } from "react";
 
 function NoteList() {
-  const notesData = [
-    {
-      id: "1",
-      title: "Get Hotel",
-      description: "Prefer Best Westerns",
-      createDate: "2021-07-11T13:32:10.000Z",
-    },
-    {
-      id: "2",
-      title: "Fill Care With Gas",
-      description: "Make sure to get Shell",
-      createDate: "2021-07-11T13:31:34.000Z",
-    },
-    {
-      id: "3",
-      title: "Pack Bathing Suit",
-      description: "Make sure to bring extra bathing suite",
-      createDate: "2021-07-11T13:33:07.000Z",
-    },
-  ];
+  const [notesData, setNotesData] = useState();
+  const [notesDataError, setNotesDataError] = useState();
+
+  useEffect(() => {
+    async function getData() {
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      try {
+        setNotesData(notes);
+      } catch (e) {
+        setNotesDataError(e);
+      }
+    }
+    getData();
+  });
+
+  if (notesDataError) {
+    return <div>error: {notesDataError}</div>;
+  }
+  if (!notesData) {
+    return <div>...loading</div>;
+  }
+  function sortByDate(a, b) {
+    const dateA = a.createDate;
+    const dateB = b.createDate;
+    return dateA > dateB ? -1 : dateA < dateB ? 1 : 0;
+  }
 
   return (
     <div className="row tab-content bg-transparent note-has-grid">
-      {notesData.map((note) => {
+      {notesData.sort(sortByDate).map((note) => {
         return <NoteCard note={note} key={note.id} />;
       })}
     </div>
