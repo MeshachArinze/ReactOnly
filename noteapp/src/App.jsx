d1a35f677426afef0efbb6788bc2b67de6e861ca
MeshachArinze <meshacharinze@gmail.com>
2023-10-21 21:24:51 +0100

    commit to main

--- noteapp/src/App.jsx
+++ noteapp/src/App.jsx
@@ -2,9 +2,10 @@ import "./styles/App.css"
 
 import NoteList from "./components/note_list";
 import useNotes from "./hooks/useNotes";
+import Menu from "./components/menu";
 
 function App() {
-  const { notesData, notesDataError } = useNotes();
+  const { notesData, notesDataError, createNote } = useNotes();
 
   if (notesDataError) {
     return <div className="container">error: {notesDataError}</div>;
@@ -13,8 +14,21 @@ function App() {
     return <div className="container">...loading</div>;
   }
 
+   function createNoteFn() {
+     const timeOfDay = new Date().toLocaleTimeString("en", {
+       hour: "2-digit",
+       minute: "2-digit",
+       second: "2-digit",
+     });
+     createNote(
+       `Note at ${timeOfDay}`,
+       `This sample note created at ${timeOfDay}`
+     );
+   }
+
   return (
     <div className="container">
+      <Menu createNoteFn={createNoteFn} />
       <NoteList notesData={notesData} />
     </div>
   );
