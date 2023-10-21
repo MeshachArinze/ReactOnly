df0c6c92b11ac28b7cbb3af3c99a34593d440035
MeshachArinze <meshacharinze@gmail.com>
2023-10-21 19:47:44 +0100

    commit to main

--- noteapp/src/App.jsx
+++ noteapp/src/App.jsx
@@ -1,12 +1,13 @@
+import "./styles/App.css"
 
+import NoteList from "./components/note_list";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <div className="container">
+      <NoteList />
+    </div>
+  );
 }
 
-export default App
+export default App;
