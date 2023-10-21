4935a3610ff4665933f8c34a478cb6705d49823b
MeshachArinze <meshacharinze@gmail.com>
2023-10-21 23:27:32 +0100

    commit to main

--- noteapp/src/App.jsx
+++ noteapp/src/App.jsx
@@ -1,35 +1,49 @@
 import "./styles/App.css"
 
+import { NotesContext } from "./context";
 import NoteList from "./components/note_list";
 import useNotes from "./hooks/useNotes";
 import Menu from "./components/menu";
 
 function App() {
-  const { notesData, notesDataError, createNote } = useNotes();
+   const contextValue = useNotes();
+  // const { notesData, notesDataError, createNote, updateNote, deleteNote } =
+  //   useNotes();
 
-  if (notesDataError) {
-    return <div className="container">error: {notesDataError}</div>;
-  }
-  if (!notesData) {
-    return <div className="container">...loading</div>;
-  }
+  // if (notesDataError) {
+  //   return <div className="container">error: {notesDataError}</div>;
+  // }
+  // if (!notesData) {
+  //   return <div className="container">...loading</div>;
+  // }
 
-   function createNoteFn() {
-     const timeOfDay = new Date().toLocaleTimeString("en", {
-       hour: "2-digit",
-       minute: "2-digit",
-       second: "2-digit",
-     });
-     createNote(
-       `Note at ${timeOfDay}`,
-       `This sample note created at ${timeOfDay}`
-     );
-   }
+    if (contextValue.notesDataError) {
+      return (
+        <div className="container">error: {contextValue.notesDataError}</div>
+      );
+    }
+    if (!contextValue.notesData) {
+      return <div className="container">...loading</div>;
+    }
+
+  //  function createNoteFn() {
+  //    const timeOfDay = new Date().toLocaleTimeString("en", {
+  //      hour: "2-digit",
+  //      minute: "2-digit",
+  //      second: "2-digit",
+  //    });
+  //    createNote(
+  //      `Note at ${timeOfDay}`,
+  //      `This sample note created at ${timeOfDay}`
+  //    );
+  //  }
 
   return (
     <div className="container">
-      <Menu createNoteFn={createNoteFn} />
-      <NoteList notesData={notesData} />
+      <NotesContext.Provider value={contextValue}>
+        <Menu />
+        <NoteList />
+      </NotesContext.Provider>
     </div>
   );
 }
--- noteapp/src/components/note_card.jsx
+++ noteapp/src/components/note_card.jsx
@@ -1,6 +1,22 @@
 import PropTypes from "prop-types";
+import { useContext } from "react";
+import { NotesContext } from "../context";
 
 function NoteCard({ note }) {
+     const { updateNote, deleteNote } = useContext(NotesContext);
+
+     function editNoteFn(noteId) {
+       updateNote(
+         noteId,
+         undefined,
+         `${note.description} : Updated` + new Date()
+       );
+     }
+
+     function deleteNoteFn(noteId) {
+       deleteNote(noteId);
+     }
+
   return (
     <div className="col-md-4 single-note-item all-category">
       <div className="card card-body">
@@ -22,6 +38,19 @@ function NoteCard({ note }) {
         <div className="note-content">
           <p className="note-inner-content text-muted">{note.description}</p>
         </div>
+
+        <div className="d-flex align-items-center">
+          <span className="mr-2">
+            <a href="#" onClick={() => deleteNoteFn(note.id)}>
+              <i className="fa fa-trash fa-lg"></i>
+            </a>
+          </span>
+          <span className="mr-2">
+            <a href="#" onClick={() => editNoteFn(note.id)}>
+              <i className="fa fa-edit fa-lg"></i>
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
@@ -32,7 +61,11 @@ NoteCard.propTypes = {
     title: PropTypes.string,
     createDate: PropTypes.string,
     description: PropTypes.string,
-  }),
+    id: PropTypes.string  }),
+
+    updateNote: PropTypes.func,
+    deleteNote: PropTypes.func
 };
+
 
 export default NoteCard;
