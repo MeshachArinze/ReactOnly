6696a096648570aad2bc2a7a2fe01d274d91ce30
MeshachArinze <meshacharinze@gmail.com>
2023-10-28 18:10:12 +0100

    commit to main

---
+++ reactauth/src/hooks/useAxiosPrivate.jsx
@@ -0,0 +1,43 @@
+import { axiosPrivate } from "../api/axios";
+import { useEffect } from "react";
+import useRefreshToken from "./useRefreshToken";
+import useAuth from "./useAuth";
+
+
+const useAxiosPrivate = () => {
+    const refresh = useRefreshToken();
+    const { auth } = useAuth();
+
+    useEffect(() => {
+        const requestIntercept = axiosPrivate.interceptors.request.use(
+            config => {
+                if (!config.headers["Authorization"]) {
+                    config.headers["Authorization"] = `Bearer ${auth?.accessToken}`;
+                }
+
+                return config;
+            }, (error) => Promise.reject(error)
+        )
+
+        const responseIntercept = axiosPrivate.interceptors.response.use(
+            response => response,
+
+            async (error) => {
+                const prevRequest = error?.config;
+                if () {
+                    
+                }
+            }
+
+        )
+
+        return () => {
+            axiosPrivate.interceptors.request.eject(requestIntercept);
+            axiosPrivate.interceptors.response.eject(responseIntercept)
+        }
+
+
+    }, [auth, refresh])
+}
+
+export default useAxiosPrivate;
